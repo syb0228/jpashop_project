70e0a4b81055a89ca4f0aaa7d4f7258556a9bc50
syb0228 <syb0228@naver.com>
2024-02-05 23:24:13 +0900

    [feat] UpdateMember api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,11 +3,10 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +17,7 @@ public class MemberApiController {
     /**
      * 등록 V1 : 요청 값으로 Member 엔티티를 직접 받음
      * @param member
-     * @return
+     * @return CreateMemberResponse
      */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -31,7 +30,7 @@ public class MemberApiController {
     /**
      * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받음
      * @param request
-     * @return
+     * @return CreateMemberResponse
      */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
@@ -44,22 +43,44 @@ public class MemberApiController {
 
     }
 
-    @Data
-    static class CreateMemberRequest {
+    /**
+     * 수정 API
+     * @param id
+     * @param request
+     * @return UpdateMemberResponse
+     */
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
 
-        private String name;
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
 
     }
 
     @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
     static class CreateMemberResponse {
-
         private Long id;
-
         public CreateMemberResponse(Long id){
             this.id = id;
         }
+    }
 
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
 
